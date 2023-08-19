e8416e7c6fa8ff2eb2bddc2eb1369496df4c3ca6
Aaron Valero <aaron.valero@outlook.com>
2023-08-19 15:06:09 +0900

    add hello world module

---
+++ 00-hello-world/00_hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // This lines is a comment, it will be ignored by the compiler
+    std::cout << "Hello, World!" << std::endl; // Ending with a semicolon is mandatory
+    return 0;
+}
