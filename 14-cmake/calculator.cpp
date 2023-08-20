15f0c7b1d437cb022a84bfeca410b7ab6e380fea
Aaron Valero <aaron.valero@outlook.com>
2023-08-20 16:35:18 +0900

    Missing calculator.cpp

---
+++ 14-cmake/calculator.cpp
@@ -0,0 +1,13 @@
+#include "calculator.h"
+
+Calculator::Calculator() {
+    // Constructor implementation
+}
+
+int Calculator::add(int a, int b) {
+    return a + b;
+}
+
+int Calculator::subtract(int a, int b) {
+    return a - b;
+}
